b350c8aa70ca1a8c2ace5a7cb609e85d8b38f9dc
gtonks <a02288558@aggies.usu.edu>
2023-04-20 19:46:03 -0600

    Improved reporting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import statistics
 
 from VariableInflation import VariableInflation
 from InflationFactor import InflationFactor
@@ -24,6 +25,18 @@ factors = [InflationFactor(VariableInflation(0.04, 0.02))]
 paths = list()
 for n_path in range(n_paths):
     paths.append(run_path(start_value, countdown, dt, factors))
+
+ends = [path[-1] for path in paths]
+deciles = statistics.quantiles(ends, n=10)
+quartiles = statistics.quantiles(ends)
+print(f"Minimum end value: {min(ends)               :.2f}")
+print(f"Lower 10%:         {deciles[0]              :.2f}")
+print(f"Lower 75%:         {quartiles[0]            :.2f}")
+print(f"Median end value:  {quartiles[1]            :.2f}")
+print(f"Upper 75%:         {quartiles[-1]           :.2f}")
+print(f"Upper 10%:         {deciles[-1]             :.2f}")
+print(f"Maximum end value: {max(ends)               :.2f}")
+print(f"Mean end value:    {statistics.mean(ends)   :.2f}")
 
 for path in paths:
     plt.plot(path)
